99d0ae82bc5da695ad777409a29a485cf9b1b9f4
mido <101448145+midoch@users.noreply.github.com>
2024-01-22 14:13:07 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,41 @@
+// api.js
+
 import axios from "axios";
 
 const baseUrl =
-  "http://192.168.8.100/api/CaPeQqc2vC7aIo7VX5xfPKx1n6ZMv-BOmk4RV1VW/lights";
+  "http://192.168.8.100/api/CaPeQqc2vC7aIo7VX5xfPKx1n6ZMv-BOmk4RV1VW";
 
 export const fetchLampState = async (lampId) => {
-  const response = await axios.get(`${baseUrl}/${lampId}`);
-  return response.data.state;
+  try {
+    const response = await axios.get(`${baseUrl}/lights/${lampId}`);
+    return response.data.state;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const updateLampState = async (lampId, payload) => {
-  await axios.put(`${baseUrl}/${lampId}/state`, payload);
+  try {
+    await axios.put(`${baseUrl}/lights/${lampId}/state`, payload);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchRooms = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}/groups`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchLampDetails = async (lampId) => {
+  try {
+    const response = await axios.get(`${baseUrl}/lights/${lampId}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
