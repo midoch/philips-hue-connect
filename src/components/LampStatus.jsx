f9b9834a90d2a4a286a31dc6cdd8aba522fda3e5
mido <101448145+midoch@users.noreply.github.com>
2024-01-23 10:52:51 +0100

    Update LampStatus.jsx

--- src/components/LampStatus.jsx
+++ src/components/LampStatus.jsx
@@ -8,9 +8,9 @@ const LampStatus = () => {
     const fetchLampData = async () => {
       try {
         // Assuming lamp ID is 6, update it as needed
-        const lampId = 6;
+        const lampId = 55;
         const lampData = await api.fetchLampState(lampId);
-        setLampState(lampData.state);
+        setLampState(lampData);
         console.log("Fetched lamp data:", lampData);
       } catch (error) {
         console.error("Error fetching lamp state:", error);
@@ -22,27 +22,21 @@ const LampStatus = () => {
 
   return (
     <div>
-      <div
-        style={{
-          width: "50px",
-          height: "50px",
-          borderRadius: "50%",
-          backgroundColor: lampState
-            ? `rgb(${Math.floor(lampState.xy[0] * 255)}, ${Math.floor(
-                lampState.xy[1] * 255
-              )}, 128)`
-            : "transparent",
-        }}
-      ></div>
+      <h2>Lamp Status</h2>
       {lampState && (
         <>
+          <div
+            style={{
+              width: "30px",
+              height: "30px",
+              borderRadius: "50%",
+              backgroundColor: `rgb(${Math.round(
+                lampState.xy[0] * 255
+              )},${Math.round(lampState.xy[1] * 255)},0)`,
+            }}
+          ></div>
           <p>Is On: {lampState.on ? "Yes" : "No"}</p>
-          {lampState.xy && (
-            <>
-              <p>Color: {JSON.stringify(lampState.xy)}</p>
-            </>
-          )}
-          {/* Add more lamp state details as needed */}
+          <p>Color: {JSON.stringify(lampState.xy)}</p>
         </>
       )}
     </div>
